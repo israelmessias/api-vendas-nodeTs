16a7e40aa6bfdc060bcf4a03725259f68cee3257
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-11-12 18:52:31 -0300

    tratamento de erro implementado

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,12 +1,31 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './http/routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(cors());
 //api trabalha com o padrão json
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    /*Erro causado pela aplicação*/
+    if (error instanceof AppError) {
+      return response.status(error.statuscode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    /*Erro causado pelo servidor*/
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 /*o listen cria a porta para o caminho*/
 app.listen(333, () => {
