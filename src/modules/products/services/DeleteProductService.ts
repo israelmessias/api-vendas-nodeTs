bcef77c1f21609cd0bab3be78a145199cd72d156
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-11-14 19:46:00 -0300

    create service product: delete

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import ProductRepository from '../typeorm/repository/ProductRespository';
+
+interface IRequest {
+  id: string;
+}
+
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const repository = getCustomRepository(ProductRepository);
+
+    const product = await repository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Produto não encontrado.');
+    }
+
+    await repository.remove(product);
+  }
+}
+
+export default DeleteProductService;
