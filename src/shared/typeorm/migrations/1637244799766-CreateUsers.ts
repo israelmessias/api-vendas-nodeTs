206092e6b81c695528e5c1fb49574ee43955de0d
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-11-18 17:51:03 -0300

    migrations usuario

---
+++ src/shared/typeorm/migrations/1637244799766-CreateUsers.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1637244799766 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            //coluna id isPrimary=primary key, generationStrategy = tipo de id
+            name: 'id',
+            type: 'uuid', //id unico universal
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'create_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'update_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
