56d03f495c903fb66164f3f8153afdd281d4155f
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-11-20 08:55:48 -0300

    Rota users

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,11 @@
 import productsRouter from '@modules/products/routes/ProductsRouter';
+import userRouter from '@modules/users/routes/UserRouter';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+
+routes.use('/users', userRouter);
 
 export default routes;
